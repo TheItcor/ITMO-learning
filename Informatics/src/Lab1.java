abcf6cec3f6148dc141056fea8f5c9705802ce0b
TheItcor <itcorsmail@gmail.com>
2025-09-28 23:56:58 +0300

    informatics: Lab1 is done
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

--- Informatics/src/Lab1.java
+++ Informatics/src/Lab1.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 class Lab1 {
     static int fibonacciNumber(int number) {
-        /* Число фибоначи по номеру*/
+        /* Поиск числа фибоначчи по индексу*/
         if (number == 1 || number == 2) return 1;
         int a = 1, b = 1, result = 0;
         for (int i = 3; i <= number; i++) {
@@ -13,29 +15,29 @@ class Lab1 {
 
 
     static int searchBigFib(int number) {
+        /* Нахождение наибольшего числа*/
         int i = 2;
         while (fibonacciNumber(i) <= number) {
             i++;
         }
-
-        // возращение предудущего числа
-        return i - 1;
+        return i - 1; // предыдущий индекс
     }
 
 
     static void translationNumber(int number) {
-        int bigFib = searchBigFib(number);
+        /* Перевод из десятичной системы счисления в систему Фибоначчи*/
+        int bigFib = searchBigFib(number) ;
 
-        boolean wasOne = false; // чтобы не было двух единиц подряд
-        for (int i = bigFib; i >= 2; i--) {
-            int f = fibonacciNumber(i);
-            if (f <= number && !wasOne) {
-                System.out.print("1");
-                number -= f;
-                wasOne = true;
-            } else {
+        for (; bigFib > 1; bigFib--) {
+
+            // Test
+            //System.out.println("F" + bigFib + " = " + fibonacciNumber(bigFib) + " ? " + number);
+
+            if (fibonacciNumber(bigFib) > number) {
                 System.out.print("0");
-                wasOne = false;
+            } else {
+                number -= fibonacciNumber(bigFib);
+                System.out.print("1");
             }
         }
 
@@ -44,30 +46,33 @@ class Lab1 {
 
 
     public static void main(String[] args) {
+//        // Tests
+//        System.out.println("F1 = " + fibonacciNumber(1));
+//        System.out.println("F2 = " + fibonacciNumber(2));
+//        System.out.println("F3 = " + fibonacciNumber(3));
+//        System.out.println("F7 = " + fibonacciNumber(7));
+//        System.out.println("F8 = " + fibonacciNumber(8));
+//        System.out.println("F9 = " + fibonacciNumber(9));
+//        System.out.println();
+//
+//        // Tests
+//        System.out.println(searchBigFib(25)); // 7
+//        System.out.println(searchBigFib(65)); // 9
+//        System.out.println(searchBigFib(7)); // 4
+//        System.out.println();
+//
+//        // Tests
+//        translationNumber(25);  // 1000101
+//        translationNumber(65);  // 100010010
+//        translationNumber(4);   // 101
+//        translationNumber(6);   // 1001
+//        translationNumber(10);  // 10010
+//        translationNumber(99);  // 1000010010
+//        translationNumber(31);  // 1010010
 
-        // Tests
-        System.out.println("F1 = " + fibonacciNumber(1));
-        System.out.println("F2 = " + fibonacciNumber(2));
-        System.out.println("F3 = " + fibonacciNumber(3));
-        System.out.println("F7 = " + fibonacciNumber(7));
-        System.out.println("F8 = " + fibonacciNumber(8));
-        System.out.println("F9 = " + fibonacciNumber(9));
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-        // Tests
-        System.out.println(searchBigFib(25)); // 7
-        System.out.println(searchBigFib(65)); // 9
-        System.out.println(searchBigFib(7)); // 4
-        System.out.println();
-
-        // Tests
-        translationNumber(25);  // 1000101
-        translationNumber(65);  // 10010010
-        translationNumber(4);   // 101
-        translationNumber(6);   // 1001
-        translationNumber(10);  // 10010
-        translationNumber(99);  // 1000010010
-        translationNumber(31);  // 1010010
-
+        translationNumber(number);
     }
 }
