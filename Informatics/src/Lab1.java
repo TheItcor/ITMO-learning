8cab5b057853cefc78d6cc997332ebbd31418578
TheItcor <itcorsmail@gmail.com>
2025-09-28 20:52:13 +0300

    informatics: lab funcs
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

--- Informatics/src/Lab1.java
+++ Informatics/src/Lab1.java
@@ -1,7 +1,25 @@
 class Lab1 {
-    fi
+    static int fibbonacyNumber(int number){
+        /* Метод, получающий н-ное число фиббоначи */
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+
+    static int searchSmallestFib(int number) {
+
+    }
+
+
+    static int translationNumber(int number) {
+
+    }
+
 
     public static void main(String[] args) {
+
+        int a = 15;
+
 
     }
 }
