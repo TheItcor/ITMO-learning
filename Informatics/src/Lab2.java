29b616fe14baf86f52d394792cf1223d9a90b097
TheItcor <itcorsmail@gmail.com>
2025-10-08 20:53:30 +0300

    Informatics -> WIP Lab2 *static methonds (1/3) *main (WIP)
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

--- Informatics/src/Lab2.java
+++ Informatics/src/Lab2.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 /* Написать программу на любом языке программирования, которая на
 вход получает набор из 7 цифр «0» и «1», записанных подряд,
@@ -8,9 +8,37 @@
 
 class Lab2 {
 
+    static byte[] byteTranlate(String message, int length) {
+        /* Метод переводящий строку в массив байтов 0 и 1 */
+        byte[] byte_message = new byte[length];
+        for (int i = 0; i < length; i++) {
+            byte_message[i] = (byte) (message.charAt(i) - '0');
+        }
+
+        return byte_message;
+    }
+
+    static int findControlPoints(int length){
+        /* Метод, находящий количество контрольных битов в сообщении */
+        int a;
+
+        return a;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String user_message = scanner.nextLine();
+
+        // Получаем массив байтов из строки, которую ввёл пользователь
+        byte length = 7; //по условиям задачи длина сообщения 7
+        byte[] byte_message = byteTranlate(user_message, length);
 
 
+
+
+        for (int i = 0; i < 7; i++) {
+            System.out.print(byte_message[i]);
+        }
 
     }
 }
