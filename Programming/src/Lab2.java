4fc04b7b22fc8269caef5a5b6ef3183d400902f2
TheItcor <itcorsmail@gmail.com>
2025-10-16 15:38:43 +0300

    Programming -> Lab2 WIP
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

---
+++ Programming/src/Lab2.java
@@ -0,0 +1,20 @@
+public class Lab2 {
+    class Pokemon {
+
+        int HP;
+        int attack;
+        int defense;
+        String special_attack;
+        String special_defense;
+        int speed;
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
