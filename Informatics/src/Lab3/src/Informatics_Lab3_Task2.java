fe6c6c9c3b4978bb0d3ee8edfa62d2e88947deac
TheItcor <itcorsmail@gmail.com>
2025-10-26 21:40:51 +0300

    Informatics -> Lab3 Task1 is done Task2 is done Task3 WIP
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

--- Informatics/src/Lab3/src/Informatics_Lab3_Task2.java
+++ Informatics/src/Lab3/src/Informatics_Lab3_Task2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // Author = None None None
 // Group = None
 // Date = 27.10.2025
@@ -7,26 +11,13 @@
 Студент Вася очень любит курс «Компьютерная безопасность». Однажды Васе
 задали домашнее задание зашифровать данные, переданные в сообщении. Недолго
 думая, Вася решил заменить все целые числа на функцию от этого числа. Функцию
-он придумал не сложную 5x^3−13 , где x −исходное число . Помогите Васе с его
+он придумал не сложную 5x^3−13, где x −исходное число. Помогите Васе с его
 домашним заданием.
 Предусмотреть ситуацию, когда в тексте будут не только целые числа, но и любые
 символы.
  */
 
-/* TO DO
-1. метод, который получает строку и находит все числа
-и засовывает их в массив
 
-2. метод, который принимает массив, шифрует, отдаёт зашифрованный массив
-
-3. метод, который получает изначальную строку и зашифрованный массив и объединяет их
-а затем отдаёт уже зашифрованную строку.
- */
-
-
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class Informatics_Lab3_Task2 {
 
@@ -57,13 +48,31 @@ class Informatics_Lab3_Task2 {
         return cryptNumbers;
     }
 
+    static String replaceNumbers(String message, ArrayList<Integer> arrayNumbers) {
+        Pattern pattern = Pattern.compile("\\d+\\.?\\d*");
+        Matcher matcher = pattern.matcher(message);
+
+        int index = 0;
+        StringBuffer result = new StringBuffer();
+
+        while (matcher.find() && index < arrayNumbers.size()) {
+            matcher.appendReplacement(result, String.valueOf(arrayNumbers.get(index)));
+            index++;
+        }
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         String test1 = "15 + 22 = 37";
+        String test2 = "6a * 9b = x";
 
-        String[] tests = {test1};
+        String[] tests = {test1, test2};
 
         for (String message : tests) {
-            System.out.println(cryptNumbers(getNumbers(message)));
+            ArrayList<Integer> newNumbers = cryptNumbers(getNumbers(message));
+            System.out.println(replaceNumbers(message, newNumbers));
         }
 
     }
