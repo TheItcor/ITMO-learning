e1bae424eab0e7a3799e1d4b6af4db7a0d3aa876
TheItcor <itcorsmail@gmail.com>
2025-10-26 21:17:02 +0300

    Informatics -> Lab3 Task1 is done Task2 WIP
    Signed-off-by: TheItcor <itcorsmail@gmail.com>

--- Informatics/src/Lab3/src/Informatics_Lab3_Task2.java
+++ Informatics/src/Lab3/src/Informatics_Lab3_Task2.java
@@ -13,9 +13,58 @@
 символы.
  */
 
+/* TO DO
+1. метод, который получает строку и находит все числа
+и засовывает их в массив
+
+2. метод, который принимает массив, шифрует, отдаёт зашифрованный массив
+
+3. метод, который получает изначальную строку и зашифрованный массив и объединяет их
+а затем отдаёт уже зашифрованную строку.
+ */
+
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Informatics_Lab3_Task2 {
 
+    static ArrayList <Integer> getNumbers(String message) {
+        // Метод, получающий строку и достаёт из него числа в массив
+        ArrayList <Integer> arrayNumbers = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(message);
+
+        while (matcher.find()) {
+            arrayNumbers.add(Integer.parseInt(matcher.group()));
+        }
+
+        return arrayNumbers;
+    }
+
+    static ArrayList<Integer> cryptNumbers(ArrayList<Integer> arrayNumbers) {
+        // Метод, получающий массив чисел, который заменяет числа на функцию от числа
+        // возвращает массив чисел от функции
+        ArrayList<Integer> cryptNumbers = new ArrayList<>();
+
+        for (int number : arrayNumbers) {
+            // Добавляем в динамический массив число от функции по очереди.
+            cryptNumbers.add((int) (Math.pow(number, 3) - 13));
+        }
+
+        return cryptNumbers;
+    }
+
     public static void main(String[] args) {
-        
+        String test1 = "15 + 22 = 37";
+
+        String[] tests = {test1};
+
+        for (String message : tests) {
+            System.out.println(cryptNumbers(getNumbers(message)));
+        }
+
     }
 }
