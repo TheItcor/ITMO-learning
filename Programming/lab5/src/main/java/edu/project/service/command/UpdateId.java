90447f547d3bc004d8b3022101a0f8495e3b0c06
TheItcor <itcorsmail@gmail.com>
2026-05-16 00:40:39 +0300

    Programming -> lab5 -> add command UpdateID

--- Programming/lab5/src/main/java/edu/project/service/command/UpdateId.java
+++ Programming/lab5/src/main/java/edu/project/service/command/UpdateId.java
@@ -1,9 +1,16 @@
 package edu.project.service.command;
 
 public class UpdateId extends Command{
-    private final String name = "update";
-    private final String description = "update id {element} : обновить значение элемента коллекции, id которого равен заданному";
-    private final boolean hasOperand = false;
+    private final static String name = "update";
+    private final static String description = "update id {element} : обновить значение элемента коллекции, id которого равен заданному";
+    private final static boolean hasOperand = true;
+
+    @Override
+    boolean execute(String Operand) {
+
+
+        return true;
+    }
 
     public UpdateId(String name, String description, boolean hasOperand) {
         super(name, description, hasOperand);
