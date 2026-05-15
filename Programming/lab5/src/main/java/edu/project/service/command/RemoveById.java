600691d5fa423fda6fa32d655193abee5106d993
TheItcor <itcorsmail@gmail.com>
2026-05-16 00:21:19 +0300

    Programming -> lab5 -> add RemoveById

--- Programming/lab5/src/main/java/edu/project/service/command/RemoveById.java
+++ Programming/lab5/src/main/java/edu/project/service/command/RemoveById.java
@@ -1,11 +1,39 @@
 package edu.project.service.command;
 
-public class RemoveById extends Command{
-    private final String name = "remove_by_id";
-    private final String description = "remove_by_id id : удалить элемент из коллекции по его id";
-    private final boolean hasOperand = true;
+import edu.project.service.collection.CollectionManager;
+import edu.project.service.collection.Ticket;
 
-    public RemoveById(String name, String description, boolean hasOperand) {
+import java.util.Set;
+
+public class RemoveById extends Command{
+    private final static String name = "remove_by_id";
+    private final static String description = "remove_by_id id : удалить элемент из коллекции по его id";
+    private final static boolean hasOperand = true;
+    private final CollectionManager collManager;
+
+
+    /**
+     * Находит по ID нужного билета (Ticket) и удаляет его из коллекции.
+     * @param ID - операнд, ID билета
+     */
+    @Override
+    boolean execute(String ID) {
+
+
+        Ticket ticket = collManager.findById(ID);
+        try {
+            collManager.deleteTicket(ticket);
+        } catch (Exception e) {
+            System.out.println("Не удалось удалить билет по ID");
+            return false;
+        }
+
+        System.out.println("Билеты на " + ticket.getName() +" успешно удалёны.");
+        return true;
+    }
+
+    public RemoveById(CollectionManager collManager) {
         super(name, description, hasOperand);
+        this.collManager = collManager;
     }
 }
